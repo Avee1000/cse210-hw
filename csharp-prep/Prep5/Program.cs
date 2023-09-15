2c70e4bbfc9b9643ab54f3f4431dd535baf21e4e
Avee1000 <eddyidahosa01@gmail.com>
2023-09-15 14:48:18 -0700

    Added function based codes

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,46 @@
 using System;
+using System.ComponentModel;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+        int squared = SquaredNumber(number);
+        DisplayResult(name, squared);
+    
+    }
+
+    static void DisplayWelcome ()
+    {
+        Console.WriteLine("Welcome to the program");
+    }
+
+    static string PromptUserName ()
+    {
+        Console.Write("Please enter your name: ");
+        string userName = Console.ReadLine();
+        return userName;
+    }
+
+    static int PromptUserNumber ()
+    {
+        Console.Write("Please enter your favourite number: ");
+        string userNumber = Console.ReadLine();
+        int conv = int.Parse(userNumber);
+        return conv;
+    }
+
+    static int SquaredNumber (int number)
+    {
+        int square = number * number;
+        return square;
+    }
+
+    static void DisplayResult (string name, int square)
+    {
+        Console.WriteLine($"Brother/Sister {name}, the square of your favourite number is: {square}");
     }
 }

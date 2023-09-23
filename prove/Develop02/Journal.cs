2b0c9e7c7d2f854d3883f3476ceafa9a2f0f009d
Avee1000 <eddyidahosa01@gmail.com>
2023-09-23 16:47:15 -0700

    Updated Journal Code

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -23,16 +23,31 @@ public class Journal
         Console.WriteLine("\nIncrease your console size to get the best view.");
 
     }
+    // public void SaveToFile(string file)
+    // {
+    //     using (StreamWriter userFile = new StreamWriter(file))
+    //     {
+    //         foreach (Entry entry in _entries)
+    //         {
+    //             userFile.WriteLine($"\nDATE: {entry._date}\nTIME: {entry._time}\nPROMPT:{entry._promptText}\nENTRY:{entry._entryText}");
+    //         }
+    //         Console.WriteLine("Your file has been saved.");
+    //     }
+    // }
+
+    //found another way that appends new stuff to the file and doesn't delete the old stuff
     public void SaveToFile(string file)
     {
-        using (StreamWriter userFile = new StreamWriter(file))
+        string fileContent = "";
+
+        foreach (Entry entry in _entries)
         {
-            foreach (Entry entry in _entries)
-            {
-                userFile.WriteLine($"\nDATE: {entry._date}\nTIME: {entry._time}\nPROMPT:{entry._promptText}\nENTRY:{entry._entryText}");
-            }
-            Console.WriteLine("Your file has been saved.");
+            fileContent += $"\nDATE: {entry._date}\nTIME: {entry._time}\n";
+            fileContent += $"PROMPT:{entry._promptText}\nENTRY:{entry._entryText}\n";
         }
+
+        File.AppendAllText(file, fileContent);
+        Console.WriteLine("Your file has been saved.");
     }
 
     public void LoadFromFile(string file)
